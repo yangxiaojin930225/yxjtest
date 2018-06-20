713bff56f505a7dbccebcac0be84b2457b833bff
yangxiaojin <1130281372@qq.com>
2018-06-20 10:20:09 +0800

    bug修改

--- app/src/main/java/vis/dvp/com/yxjtest/demo/view/MainActivity.java
+++ app/src/main/java/vis/dvp/com/yxjtest/demo/view/MainActivity.java
@@ -1,16 +1,12 @@
 package vis.dvp.com.yxjtest.demo.view;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-
-import com.wuxiaolong.androidmvpsample.common.mvp.view.MvpActivity;
-import com.wuxiaolong.androidmvpsample.demo.module.MainModel;
-import com.wuxiaolong.androidmvpsample.demo.presenter.MainPresenter;
-import com.wuxiaolong.androidmvpsample.demo.view.MainView;
 
 import vis.dvp.com.yxjtest.R;
 
-public class MainActivity extends MvpActivity<MainPresenter> implements MainView {
+public class MainActivity extends AppCompatActivity {
 
     private TextView text;
 
@@ -19,28 +15,5 @@ public class MainActivity extends MvpActivity<MainPresenter> implements MainView
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         text = findViewById(R.id.text);
-        setCenterText("123");
-    }
-    @Override
-    protected MainPresenter createPresenter() {
-        return new MainPresenter(this);
-    }
-
-
-    @Override
-    public void getDataSuccess(MainModel model, String type) {
-        //接口成功回调
-        dataSuccess(model);
-    }
-
-    @Override
-    public void getDataFail(String msg) {
-        toastShow("请求失败");
-
-    }
-
-
-    private void dataSuccess(MainModel model) {
-      toastShow("请求成功");
     }
 }
