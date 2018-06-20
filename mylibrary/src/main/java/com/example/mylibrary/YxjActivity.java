5c34ec3a09133529a15c6973b118a06e74c9c4e5
yangxiaojin <1130281372@qq.com>
2018-06-20 15:32:52 +0800

    bug修改7

---
+++ mylibrary/src/main/java/com/example/mylibrary/YxjActivity.java
@@ -0,0 +1,19 @@
+package com.example.mylibrary;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 类：
+ * 作者：魏兴洲
+ * 时间：2018/6/20 15:27
+ * 功能描述:yxjtest
+ */
+public class YxjActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
